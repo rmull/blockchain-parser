dcd169849df44343a8515b5e980d20d8943d9e6b
Ryan Mullen <rmmullen@gmail.com>
2014-10-20 00:55:16 -0400

    Able to get the parser to see each block file

--- main.c
+++ main.c
@@ -1,4 +1,11 @@
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include <errno.h>
+#include <fcntl.h>
+#include <dirent.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "format.h"
 #include "parse.h"
@@ -8,9 +15,51 @@
 int
 main(int argc, char *argv[])
 {
+    DIR *datadir = NULL;
+    struct dirent *dp = NULL;
+    int fd;
+    int ret = 0;
+
+    /* Open the blockchain data directory */
+    datadir = opendir(BLOCKCHAIN_PATH);
+    if (datadir == NULL) {
+        perror("opendir");
+        ret = 1;
+        goto exit;
+    }
     printf("Parsing from %s\n", BLOCKCHAIN_PATH);
+    chdir(BLOCKCHAIN_PATH);
 
-    parse();
+    do {
+        errno = 0;
+        
+        dp = readdir(datadir);
+        if (dp != NULL) {
+            if (memcmp(dp->d_name, "blk", 3) == 0) {
+                
+                /* 
+                 * NOTE: We are not bothering to sort by filename because we
+                 * assume the blockchain files have ascending inode numbers
+                 */
+                /* printf("%d\n", dp->d_ino); */
 
-    return 0; 
+                fd = open(dp->d_name, O_RDONLY);
+                if (fd < 0) {
+                    perror("open");
+                } else {
+                    parse(fd);
+                    close(fd);
+                }
+            }
+        } else {
+            if (errno != 0) {
+                perror("readdir");
+            }
+        }
+    } while (dp != NULL && datadir != NULL);
+
+    
+exit:
+    closedir(datadir);
+    return ret; 
 }
--- parse.c
+++ parse.c
@@ -1,10 +1,24 @@
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include <errno.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #include "format.h"
 #include "parse.h"
 
 void
-parse(void)
+parse(int blkfd)
 {
-    ;
+    struct stat sb;
+    int ret;
+
+    ret = fstat(blkfd, &sb);
+    if (ret < 0) {
+        perror("stat");
+    } else {
+        printf("%d\n", sb.st_ino);
+    }
+
 }
