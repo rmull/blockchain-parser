8624a471afb1602c2860546d1fc23e4fef372950
Ryan Mullen <rmmullen@gmail.com>
2014-10-22 23:56:44 -0400

    Pass datadir on CLI instead of via #define

--- main.c
+++ main.c
@@ -10,8 +10,6 @@
 #include "format.h"
 #include "parse.h"
 
-#define BLOCKCHAIN_PATH "/raid/btc/blocks"
-
 int
 main(int argc, char *argv[])
 {
@@ -21,54 +19,65 @@ main(int argc, char *argv[])
     int fd;
     int ret = 0;
 
-    /* Open the blockchain data directory */
-    datadir = opendir(BLOCKCHAIN_PATH);
-    if (datadir == NULL) {
-        perror("opendir");
+    if (argc == 2) {
+
+        /* Open the blockchain data directory */
+        datadir = opendir(argv[1]);
+        if (datadir == NULL) {
+            perror("opendir");
+            ret = 1;
+
+        } else {
+
+            chdir(argv[1]);
+
+            do {
+                errno = 0;
+                
+                dp = readdir(datadir);
+                if (dp != NULL) {
+                    if (memcmp(dp->d_name, "blk", 3) == 0) {
+                        
+                        /* 
+                         * NOTE: We are not bothering to sort by filename because we
+                         * assume the blockchain files have ascending inode numbers due
+                         * to the way they were created. Other sources may require more
+                         * sorting.
+                         */
+                        /* printf("%d\n", dp->d_ino); */
+
+                        fd = open(dp->d_name, O_RDONLY);
+                        if (fd < 0) {
+                            perror("open");
+                        } else {
+                            ret = fstat(fd, &sb);
+                            if (ret < 0) {
+                                perror("stat");
+                            } else {
+                                /* printf("%s\n", dp->d_name); */
+                                parse(fd, sb.st_size);
+                            }
+                            close(fd);
+                        }
+                    }
+                } else {
+                    if (errno != 0) {
+                        perror("readdir");
+                    }
+                }
+            } while (dp != NULL && datadir != NULL);
+
+        }
+        
+        closedir(datadir);
+
+    } else {
         ret = 1;
-        goto exit;
     }
 
-    chdir(BLOCKCHAIN_PATH);
+    if (ret != 0) {
+        printf("Usage: %s /path/to/blockchain/datadir\n", argv[0]);
+    }
 
-    do {
-        errno = 0;
-        
-        dp = readdir(datadir);
-        if (dp != NULL) {
-            if (memcmp(dp->d_name, "blk", 3) == 0) {
-                
-                /* 
-                 * NOTE: We are not bothering to sort by filename because we
-                 * assume the blockchain files have ascending inode numbers due
-                 * to the way they were created. Other sources may require more
-                 * sorting.
-                 */
-                /* printf("%d\n", dp->d_ino); */
-
-                fd = open(dp->d_name, O_RDONLY);
-                if (fd < 0) {
-                    perror("open");
-                } else {
-                    ret = fstat(fd, &sb);
-                    if (ret < 0) {
-                        perror("stat");
-                    } else {
-                        /* printf("%s\n", dp->d_name); */
-                        parse(fd, sb.st_size);
-                    }
-                    close(fd);
-                }
-            }
-        } else {
-            if (errno != 0) {
-                perror("readdir");
-            }
-        }
-    } while (dp != NULL && datadir != NULL);
-
-    
-exit:
-    closedir(datadir);
     return ret; 
 }
